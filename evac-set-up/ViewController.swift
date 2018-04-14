74bec86325b6307922a681eb38f5a75d1a95e7a8
Ruochun Wang <jizhidebaozi@n3-87-30.dhcp.drexel.edu>
2018-04-14 12:29:23 -0400

    Initial Commit

---
+++ evac-set-up/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  evac-set-up
+//
+//  Created by Ruochun Wang on 4/14/18.
+//  Copyright © 2018 Ruochun Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
