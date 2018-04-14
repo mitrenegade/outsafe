23136e80473352e0e08682089eb707936ed1ea1e
Ruochun Wang <jizhidebaozi@n3-87-30.dhcp.drexel.edu>
2018-04-14 14:32:51 -0400

    addedlocationmanager

--- evac-set-up/ViewController.swift
+++ evac-set-up/ViewController.swift
@@ -7,18 +7,60 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    var locationManager: CLLocationManager!
+    
+    @IBOutlet weak var saveButton: UIButton!
+    @IBOutlet weak var longitude: UILabel!
+    @IBOutlet weak var latitude: UILabel!
+  
+    
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse || status == .authorizedAlways {
+            print("location status changed")
+            locationManager.startUpdatingLocation()
+        }
+        else if status == .denied {
+            print("Authorization is not available")
+        }
+        else {
+            print("status unknown")
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager,
+                         didUpdateLocations locations: [CLLocation]){
+        var loc=locations[0]
+        latitude.text="\(loc.coordinate.latitude)"
+        longitude.text="\(loc.coordinate.longitude)"
+        print(loc.coordinate.latitude)
+        print(loc.coordinate.longitude)
+    }
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy=kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+        locationManager.startUpdatingLocation()
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    
 
 
 }
